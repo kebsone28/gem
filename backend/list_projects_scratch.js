625f9cdc0dec5810163b52e2554ae1bb950b7cb4
Your Name <partener@hotmail.com>
2026-05-17 14:29:43 +0000

    update scratch script to ES modules

--- backend/list_projects_scratch.js
+++ backend/list_projects_scratch.js
@@ -1,7 +1,10 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+import { basePrisma as prisma } from './src/core/utils/prisma.js';
+
 prisma.project.findMany().then(projects => {
   console.log('--- PROJECTS ON VPS DB ---');
   console.log(JSON.stringify(projects.map(p => ({ id: p.id, name: p.name, organizationId: p.organizationId })), null, 2));
   process.exit(0);
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
 });
